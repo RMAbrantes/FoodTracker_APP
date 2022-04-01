40fdb3915a579b84eded8d35a2f02ac67d50e00a
RMA <t_r_magalhaes@hotmail.com>
2022-04-01 16:51:08 +0100

    AddIdentity

--- FoodTracker_APP/Globals.cs
+++ FoodTracker_APP/Globals.cs
@@ -10,18 +10,24 @@ global using FoodTracker_APP.Data.Entities;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Identity.UI.Services;
 global using Microsoft.AspNetCore.Mvc.RazorPages;
 global using Microsoft.AspNetCore.Mvc.Rendering;
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.WebUtilities;
 
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Design;
 global using Microsoft.EntityFrameworkCore.Infrastructure;
 global using Microsoft.EntityFrameworkCore.Migrations;
 
+
 global using System.ComponentModel;
 global using System.ComponentModel.DataAnnotations;
 global using System.Diagnostics;
 global using System.IO;
+global using System.Text;
+global using System.Text.Encodings.Web;
 
 global using UnitsNet;
 
